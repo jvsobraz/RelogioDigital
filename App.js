388d66c212d8bf26a0623470b54712eb04a21be8
jvsobraz <jvsobraz@hotmail.com>
2023-04-09 00:20:46 -0300

    Javascript

---
+++ App.js
@@ -0,0 +1,68 @@
+
+//E, por fim, você pode adicionar o seguinte JavaScript para implementar a lógica do relógio, cronômetro e temporizador:
+
+// Obtém os elementos DOM
+const timeElement = document.getElementById("time");
+const dateElement = document.getElementById("date");
+const timezoneSelect = document.getElementById("timezone-select");
+let timerInterval;
+
+// Função para obter a data e hora atual
+function getCurrentTime() {
+  const currentTime = new Date();
+  return currentTime;
+}
+
+// Função para atualizar a exibição da hora e data
+function updateClock() {
+  const currentTime = getCurrentTime();
+  const options = { weekday: "long", month: "long", day: "numeric", year: "numeric" };
+  const timeString = currentTime.toLocaleTimeString();
+  const dateString = currentTime.toLocaleDateString(undefined, options);
+  timeElement.textContent = timeString;
+  dateElement.textContent = dateString;
+}
+
+// Função para atualizar o fuso horário
+function changeTimezone() {
+  const timezoneOffset = parseInt(timezoneSelect.value);
+  const currentTime = getCurrentTime();
+  const timezoneTime = new Date(currentTime.getTime() + timezoneOffset * 60 * 60 * 1000);
+  timeElement.textContent = timezoneTime.toLocaleTimeString();
+  dateElement.textContent = timezoneTime.toLocaleDateString(undefined, options);
+}
+
+// Funções para o temporizador
+function startTimer() {
+  let seconds = 0;
+  timerInterval = setInterval(() => {
+    seconds++;
+    timeElement.textContent = formatTime(seconds);
+  }, 1000);
+}
+
+function stopTimer() {
+  clearInterval(timerInterval);
+}
+
+function resetTimer() {
+  stopTimer();
+  timeElement.textContent = "00:00:00";
+}
+
+// Função para formatar o tempo no formato HH:MM:SS
+function formatTime(seconds) {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
+  return `${padZero(hours)}:${padZero(minutes)}:${padZero(remainingSeconds)}`;
+}
+
+// Função para adicionar zero à esquerda de números menores que 10
+function padZero(number) {
+  return number < 10 ? `0${number}` : number;
+}
+
+// Inicializa o relógio
+updateClock();
+
